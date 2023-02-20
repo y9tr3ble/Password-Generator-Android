6894831832a3abaaddc3a282e4ac3b533bc9972d
Tr3bleee <Tr3ble@outlook.com>
2023-02-20 15:05:01 +0200

    Version 1.8 Update Design(Material You) Added dark theme Refactoring code

--- app/src/main/java/com/tr3ble/passwordgenerator/PasswordGenerator.kt
+++ app/src/main/java/com/tr3ble/passwordgenerator/PasswordGenerator.kt
@@ -1,68 +1,64 @@
 package com.tr3ble.passwordgenerator
 // hmm...
-    import android.annotation.SuppressLint
-    import android.content.ClipData
-    import android.content.ClipboardManager
-    import android.os.Bundle
-    import android.view.View
-    import android.widget.Button
-    import android.widget.TextView
-    import android.widget.Toast
-    import androidx.appcompat.app.AppCompatActivity
-    import com.google.android.material.color.DynamicColors
-    import com.google.android.material.slider.Slider
-    import com.tr3ble.passwordgenerator.R.*
-    import kotlin.random.Random
+import android.annotation.SuppressLint
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.google.android.material.color.DynamicColors
+import com.google.android.material.slider.Slider
+import com.tr3ble.passwordgenerator.databinding.ActivityMainBinding
+import kotlin.random.Random
 
-    class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
 
-        private lateinit var passwordTextView: TextView
-        private lateinit var passwordLengthSlider: Slider
-        private lateinit var generateButton: Button
-        private lateinit var copyButton: Button
+    private lateinit var binding: ActivityMainBinding
 
-        @SuppressLint("SetTextI18n")
-        override fun onCreate(savedInstanceState: Bundle?) {
-            DynamicColors.applyToActivityIfAvailable(this)
-            super.onCreate(savedInstanceState)
-            setContentView(layout.activity_main)
+    @SuppressLint("SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        DynamicColors.applyToActivityIfAvailable(this)
 
-            passwordTextView = findViewById(id.password_text_view)
-            passwordLengthSlider = findViewById(id.password_length_seek_bar)
-            generateButton = findViewById(id.generate_button)
-            copyButton = findViewById(id.copy_button)
+        super.onCreate(savedInstanceState)
 
-            passwordLengthSlider.addOnChangeListener(object : Slider.OnChangeListener {
-                override fun onValueChange(slider: Slider, value: Float, fromUser: Boolean) {
-                    passwordTextView.text = "Password length: ${value.toInt()}"
-                }
-            })
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-            generateButton.setOnClickListener {
-                generatePassword()
+        binding.passwordLengthSeekBar.addOnSliderTouchListener(object :
+            Slider.OnSliderTouchListener {
+            override fun onStartTrackingTouch(slider: Slider) {}
+
+            override fun onStopTrackingTouch(slider: Slider) {
+                binding.generateButton.callOnClick()
             }
+        })
 
-            copyButton.setOnClickListener {
-                copyPasswordToClipboard()
-            }
+        binding.passwordLengthSeekBar.addOnChangeListener { _, value, _ ->
+            binding.passwordLengthTextView.text = "Password length: ${value.toInt()}"
         }
-
-        private fun generatePassword() {
+// я люблю чебуреки
+        binding.generateButton.setOnClickListener {
             val random = Random
-            val passwordLength = passwordLengthSlider.value.toInt()
+            val passwordLength = binding.passwordLengthSeekBar.value.toInt()
             val password = CharArray(passwordLength) {
                 val ranges = listOf('a'..'z', 'A'..'Z', '0'..'9' )
                 val range = ranges.random(random)
                 range.random(random)
             }
-            passwordTextView.text = "Password: ${password.joinToString("")}"
-            copyButton.visibility = View.VISIBLE
+
+            binding.passwordTextView.text = "Password: ${password.joinToString("")}"
+            binding.copyButton.visibility = View.VISIBLE
         }
 
-        private fun copyPasswordToClipboard() {
-            val clipboard = getSystemService(CLIPBOARD_SERVICE) as ClipboardManager
-            val clip = ClipData.newPlainText("Password", passwordTextView.text.toString().substring(10))
-            Toast.makeText(this, "Password copied to clipboard", Toast.LENGTH_SHORT).show()
-            clipboard.setPrimaryClip(clip)
+        binding.copyButton.setOnClickListener {
+            val clipboard = ContextCompat.getSystemService(applicationContext, ClipboardManager::class.java)
+            val clip = ClipData.newPlainText("Password", binding.passwordTextView.text.toString().substring(10))
+            if (clipboard != null) {
+                clipboard.setPrimaryClip(clip)
+                Toast.makeText(this, "Password copied to clipboard", Toast.LENGTH_SHORT).show()
+            }
         }
     }
+}
