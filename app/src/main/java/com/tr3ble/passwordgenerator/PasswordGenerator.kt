b34c8503279f3bea4aff6aab13cd603f7b48749e
Tr3bleee <Tr3ble@outlook.com>
2023-02-25 01:29:13 +0200

    Version 1.9.5 Added switches for using numbers and special characters Added password generation animation

--- app/src/main/java/com/tr3ble/passwordgenerator/PasswordGenerator.kt
+++ app/src/main/java/com/tr3ble/passwordgenerator/PasswordGenerator.kt
@@ -1,9 +1,10 @@
 package com.tr3ble.passwordgenerator
-// hmm...
+
 import android.annotation.SuppressLint
 import android.content.ClipData
 import android.content.ClipboardManager
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -13,9 +14,13 @@ import com.google.android.material.slider.Slider
 import com.tr3ble.passwordgenerator.databinding.ActivityMainBinding
 import kotlin.random.Random
 
+@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    private var useNumbers: Boolean = false
+    private var useSpecialChars: Boolean = false
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,18 +43,37 @@ class MainActivity : AppCompatActivity() {
         binding.passwordLengthSeekBar.addOnChangeListener { _, value, _ ->
             binding.passwordLengthTextView.text = "Password length: ${value.toInt()}"
         }
-// я люблю чебуреки
+
         binding.generateButton.setOnClickListener {
             val random = Random
             val passwordLength = binding.passwordLengthSeekBar.value.toInt()
+            val ranges = mutableListOf('a'..'z', 'A'..'Z')
+            if (useNumbers) {
+                ranges.add('0'..'9')
+            }
+            if (useSpecialChars) {
+                ranges.add('!'..'/')
+            }
             val password = CharArray(passwordLength) {
-                val ranges = listOf('a'..'z', 'A'..'Z', '0'..'9' )
                 val range = ranges.random(random)
                 range.random(random)
             }
 
-            binding.passwordTextView.text = "Password: ${password.joinToString("")}"
-            binding.copyButton.visibility = View.VISIBLE
+            binding.passwordTextView.text = "Password: "
+            binding.copyButton.visibility = View.INVISIBLE
+            var currentIndex = 0
+            val handler = Handler()
+            handler.postDelayed(object : Runnable {
+                override fun run() {
+                    if (currentIndex < password.size) {
+                        binding.passwordTextView.append(password[currentIndex].toString())
+                        currentIndex++
+                        handler.postDelayed(this, 50)
+                    } else {
+                        binding.copyButton.visibility = View.VISIBLE
+                    }
+                }
+            }, 50)
         }
 
         binding.copyButton.setOnClickListener {
@@ -59,6 +83,14 @@ class MainActivity : AppCompatActivity() {
                 clipboard.setPrimaryClip(clip)
                 Toast.makeText(this, "Password copied to clipboard", Toast.LENGTH_SHORT).show()
             }
+        }
+
+        binding.useNumbersSwitch.setOnCheckedChangeListener { _, isChecked ->
+            useNumbers = isChecked
+        }
+
+        binding.useSpecialCharsSwitch.setOnCheckedChangeListener { _, isChecked ->
+            useSpecialChars = isChecked
         }
     }
 }
